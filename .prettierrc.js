a55558a04e60a490d43d069b4c4f7acf6e000eda
brachkow <33203187+brachkow@users.noreply.github.com>
2022-06-23 07:10:47 +0400

    build: prettier added

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'css',
+  vueIndentScriptAndStyle: true,
+};
