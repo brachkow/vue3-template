6fb68199c10493d19697ca246e59c2569814b8a6
brachkow <33203187+brachkow@users.noreply.github.com>
2022-07-18 13:42:14 +0200

    feat: nuxt-like route name generator

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,31 @@
+import { Component } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const HomePage = () => import('./pages/HomePage/HomePage.vue');
 
-const routes = [{ path: '/', component: HomePage }];
+class Route {
+  path: string;
+  component: Component;
+
+  constructor(path: string, component: Component) {
+    this.path = path;
+    this.component = component;
+  }
+
+  get name() {
+    return this.path
+      .split('')
+      .map((char: string, index: number) => {
+        if (char === '/' && index === 0) return '';
+        if (char === '/') return '-';
+        if (char === ':') return '';
+        return char;
+      })
+      .join('');
+  }
+}
+
+const routes = [new Route('/', HomePage)];
 
 const router = createRouter({
   history: createWebHistory(),
